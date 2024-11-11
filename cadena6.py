26b7e07c76e31880f613c6ee423fc932e0fb7a9d
MurielAndy <amurielcab@educacion.navarra.es>
2024-11-11 22:37:21 +0100

    cadena6.py

---
+++ cadena6.py
@@ -0,0 +1,5 @@
+# Solicitar frase y vocal
+frase = input("Introduce una frase:")
+vocal = input("Introduce una vocal:")
+frase_modificada = frase.replace(vocal, vocal.upper())
+print("Frase modificada:", frase_modificada)
